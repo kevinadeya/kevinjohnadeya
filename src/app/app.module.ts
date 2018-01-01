57d00191d290fc6887a30d94532734d12b6cdacf
kevinadeya <adeyakevin@yahoo.com>
2018-01-01 20:03:47 +0100

    second commit with error tabs page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { ProjectsPage } from '../pages/projects/projects';
+import { SchoolservicePage } from '../pages/schoolservice/schoolservice';
+import { JoinPage } from '../pages/join/join';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,6 +20,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    ProjectsPage,
+    SchoolservicePage,
+    JoinPage,
     TabsPage
   ],
   imports: [
@@ -29,6 +35,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    ProjectsPage,
+    SchoolservicePage,
+    JoinPage,
     TabsPage
   ],
   providers: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ProjectsPage} from '../projects/projects';
+import {SchoolservicePage} from '../schoolservice/schoolservice';
+import {JoinPage} from '../join/join';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,21 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  inter() {
+    //alert("Hello projects");
+    this.navCtrl.setRoot(ProjectsPage);
+  }
+
+  school() {
+  // alert("School");
+    this.navCtrl.setRoot(SchoolservicePage);
+  }
+
+  join() {
+    //alert("Joim");
+    this.navCtrl.setRoot(JoinPage);
   }
 
 }
