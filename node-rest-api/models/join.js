298126338407694875ffd7cb4b87c08325370980
kevinadeya <adeyakevin@yahoo.com>
2018-01-22 09:46:31 +0100

    changes in join and config

--- node-rest-api/models/join.js
+++ node-rest-api/models/join.js
@@ -3,31 +3,31 @@ var mongoose = require('mongoose');
 var JoinCrossing = mongoose.model('JoinCrossing', {
   name: {
     type: String,
-    required: true,
+    //required: true,
     minlength: 1,
     trim: true
   },
   email: {
     type: String,
-    required: true,
+    //required: true,
     minlength: 1,
     trim: true
   },
   birth: {
     type: String,
-    required: true,
+    //required: true,
     minlength: 1,
     trim: true
   },
   city: {
     type: String,
-    required: true,
+    //required: true,
     minlength: 1,
     trim: true
   },
   type: {
     type: String,
-    required: true,
+    //required: true,
     minlength: 1,
     trim: true
   },
