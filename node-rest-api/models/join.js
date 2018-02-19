32d9801ef37db4fa75590bf65f960c0f1f0f0231
kevinadeya <adeyakevin@yahoo.com>
2018-02-19 11:26:00 +0100

    testing packages installed

--- node-rest-api/models/join.js
+++ node-rest-api/models/join.js
@@ -1,48 +1,27 @@
 var mongoose = require('mongoose');
-
 var JoinCrossing = mongoose.model('JoinCrossing', {
   name: {
     type: String,
-<<<<<<< HEAD
-    //required: true,
-=======
->>>>>>> d07136d80b50545359fa2f8bba10a259e1fb7db6
     minlength: 1,
     trim: true
   },
   email: {
     type: String,
-<<<<<<< HEAD
-    //required: true,
-=======
->>>>>>> d07136d80b50545359fa2f8bba10a259e1fb7db6
     minlength: 1,
     trim: true
   },
   birth: {
     type: String,
-<<<<<<< HEAD
-    //required: true,
-=======
->>>>>>> d07136d80b50545359fa2f8bba10a259e1fb7db6
     minlength: 1,
     trim: true
   },
   city: {
     type: String,
-<<<<<<< HEAD
-    //required: true,
-=======
->>>>>>> d07136d80b50545359fa2f8bba10a259e1fb7db6
     minlength: 1,
     trim: true
   },
   type: {
     type: String,
-<<<<<<< HEAD
-    //required: true,
-=======
->>>>>>> d07136d80b50545359fa2f8bba10a259e1fb7db6
     minlength: 1,
     trim: true
   },
@@ -51,5 +30,4 @@ var JoinCrossing = mongoose.model('JoinCrossing', {
       default: false
   }
 });
-
 module.exports = {JoinCrossing};
